21a2da393f1185dfde447055408dff0e5870b8b2
canosaer <67963828+canosaer@users.noreply.github.com>
2022-11-09 14:55:51 -0500

    removed featured programs

--- src/api/featured-program/controllers/featured-program.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * featured-program controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::featured-program.featured-program');
--- src/api/featured-program/services/featured-program.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * featured-program service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::featured-program.featured-program');
